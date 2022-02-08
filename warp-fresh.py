d10ae99409c6a78a59899aa7c782250c012f4be3
eternity5 <74863705+eternity5@users.noreply.github.com>
2022-02-09 02:37:53 +0800

    Update warp-fresh.py

--- warp-fresh.py
+++ warp-fresh.py
@@ -2,17 +2,23 @@ import os
 import time
 import requests
 
-n = 10
-while n > 0:
-    n = n - 1
-    os.system("systemctl restart wg-quick@wg0")
-    time.sleep(3)
-    res = requests.get("https://www.cloudflare.com/cdn-cgi/trace")
-    if res.status_code == 200 and "warp=on" in res.text:
-        print("Succeed!")
-        break
+
+def warp_fresh():
+    n = 10
+    while n > 0:
+        n = n - 1
+        os.system("systemctl restart wg-quick@wg0")
+        time.sleep(3)
+        res = requests.get("https://www.cloudflare.com/cdn-cgi/trace")
+        if res.status_code == 200 and "warp=on" in res.text:
+            break
+        else:
+            continue
     else:
-        continue
-else:
-    requests.get("https://api.day.app/mXod6Bg3mnGbvXXR7edQoH/Failure!")
-    
+        requests.get("https://api.day.app/mXod6Bg3mnGbvXXR7edQoH/更换ip失败")
+        
+        
+if __name__ == '__main__':
+    while True:
+        warp_fresh()
+        time.sleep(5000)
